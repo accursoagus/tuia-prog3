68ca4a8655b81357bba4022eaa90a871b83a9a8d
accursoagus <accursoagus@gmail.com>
2025-05-08 22:37:33 -0300

    TSP: implementación HillClimbingReset

--- tp-tsp/search.py
+++ tp-tsp/search.py
@@ -81,11 +81,74 @@ class HillClimbing(LocalSearch):
 
 class HillClimbingReset(LocalSearch):
     """Algoritmo de ascension de colinas con reinicio aleatorio."""
+    def __init__(self):
+         super().__init__()
+         self.max_reset = 100
+    
+    def solve(self, problem: OptProblem):
+        start = time()
+        self.niters = 0
+
+        actual = problem.init
+        value = problem.obj_val(problem.init)
+        self.niters += 1
+        best_tour = actual
+        best_value = value
+
+        for _ in range(self.max_reset):
+            while True:
+                    act, succ_val = problem.max_action(actual)
+                    if succ_val <= value:
+                        break
+                    actual = problem.result(actual, act)
+                    value = succ_val
+                    self.niters += 1
+
+            if value > best_value:
+                best_value = value
+                best_tour = actual
+
+            actual = problem.random_reset()
+            value = problem.obj_val(actual)
+
+        self.tour = best_tour
+        self.value = best_value
+        end = time()
+        self.time = end - start
 
     # COMPLETAR
 
 
 class Tabu(LocalSearch):
     """Algoritmo de busqueda tabu."""
+''' def solve(self, problem: OptProblem, tenure=10, max_iters=100):
+        start = time()
+        actual = problem.init
+        value = problem.obj_val(actual)
+        best_tour = actual
+        best_value = value
 
+        tabu_list = []
+        self.niters = 0
+
+        while self.niters < max_iters:
+            act, succ_val = problem.max_action(actual, tabu_list)
+            succ = problem.result(actual, act)
+
+            # Update tabu list
+            tabu_list.append(act)
+            if len(tabu_list) > tenure:
+                tabu_list.pop(0)
+                actual = succ
+                value = succ_val
+                self.niters += 1
+
+            if value > best_value:
+                best_value = value
+                best_tour = actual
+
+        self.tour = best_tour
+        self.value = best_value
+        end = time()
+        self.time = end - start'''
     # COMPLETAR
